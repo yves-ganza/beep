e6a2eebfe299be6f0cba06057e281527c9c37548
lebonYG <lebonganza@outlook.com>
2021-02-18 22:26:50 -0500

    create frontend components and cleanup landing page

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header(props){
+    return(
+        <div>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/profile">Profile</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
